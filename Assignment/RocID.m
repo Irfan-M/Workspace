7919d6d93d519344089a4b89e3f4e15583b483d3
Irfan-M <rulerofmeasurah@gmail.com>
2014-12-02 13:59:46 +0000

    Fix for RocID and adjusted part1 of script

--- Assignment/RocID.m
+++ Assignment/RocID.m
@@ -1,23 +1,20 @@
-function [specificity,sensitivity] = RocID(TrainingImage,TestingImage)
+function m = RocID(TestingImage,TrainingImage)
 
 % Calculates the sensitivity index (for the Y axis).
 %and the (1- specificity) index (for the X axis).
 
 % PLEASE USE BINARY IMAGES FOR THIS FUNCTION!
 
-im = imread(['',TrainingImage]);
-image = im(:,:,2); 
-image = double(image);
-image = im2bw(image,greythresh(image));
 
-
-TP = sum((image == 1) & (TestingImage == 1))
-TN = sum((image == 0) & (TestingImage == 0))
-FP = sum((image == 0) & (TestingImage == 1))
-FN = sum((image == 1) & (TestingImage == 0))
+TP = sum(sum((TrainingImage == 1) & (TestingImage == 1)))
+TN = sum(sum((TrainingImage == 0) & (TestingImage == 0)))
+FP = sum(sum((TrainingImage == 0) & (TestingImage == 1)))
+FN = sum(sum((TrainingImage == 1) & (TestingImage == 0)))
 
 sensitivity = TP / (TP + FN)
 specificity = 1 - (TN / (TN + FP))
+
+m = [specificity;sensitivity] 
 
 end
 
